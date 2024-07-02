fbe255613a4ba00f8a8f7f1c1c297bb02ac37c1e
Andy Chen <chenyangpoppin@hotmail.com>
2024-07-02 12:12:22 +0800

    Sample output

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
+"""
+Sample png output:
+What do you do in the mornings? page.tsx:86
+- Do you like to get up early in the morning?
+- Is breakfast important?
+
+Topic: Describe a person who likes to buy goods with low  page.tsx:93
+prices. You should say:
+- who this person is
+what this person likes to buy
+where this person likes to buy things
+and explain why this person likes cheap goods
+"""
 import pytesseract
 from PIL import Image
 
